ba812af763d1b36fc81f04e086086a37531680d2
coder <dnstylish@gmail.com>
2022-07-02 23:15:20 +0700

    Initial Commit

---
+++ Shared/Analytics_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Analytics_PageApp.swift
+//  Shared
+//
+//  Created by Yuan on 02/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Analytics_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
